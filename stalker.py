a3579613a4fbf18635b3a712d074de8862cdb9da
Kaustubha DS <dskaustubh@gmail.com>
2020-01-19 11:23:04 +0530

    stage 1 complete

--- stalker.py
+++ stalker.py
@@ -3,3 +3,9 @@ import bs4 as bs
 driver = webdriver.Chrome()
 url="http://exam.msrit.edu/"
 driver.get(url)
+usn=driver.find_elements_by_id("usn")[0]
+usn.send_keys("1MS18CS050")
+cap=driver.find_element_by_id("osolCatchaTxt0")
+cap.send_keys("eden")
+gobtn=driver.find_element_by_class_name("buttongo")
+gobtn.click()
