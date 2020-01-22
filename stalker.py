302f46c5f6ae010a34dbd3f359815ffc0fc2c43c
Kaustubha DS <dskaustubh@gmail.com>
2020-01-22 20:12:11 +0530

    stage 2

--- stalker.py
+++ stalker.py
@@ -1,11 +1,27 @@
 from selenium import webdriver
 import bs4 as bs
+import time
+
 driver = webdriver.Chrome()
 url="http://exam.msrit.edu/"
 driver.get(url)
-usn=driver.find_elements_by_id("usn")[0]
-usn.send_keys("1MS18CS050")
-cap=driver.find_element_by_id("osolCatchaTxt0")
-cap.send_keys("eden")
-gobtn=driver.find_element_by_class_name("buttongo")
-gobtn.click()
+USN="1MS19CS"
+
+capip=input("Enter The Captcha: ")
+capip=capip.upper()
+for i in range (1,150):
+    time.sleep(1.5)
+    usn=driver.find_elements_by_id("usn")[0]
+    cap=driver.find_element_by_id("osolCatchaTxt0")
+    gobtn=driver.find_element_by_class_name("buttongo")
+    usn.send_keys(USN+str(i).zfill(3))
+    cap.send_keys(capip)
+    gobtn.click()
+    try:
+        sgpa=driver.find_element_by_xpath('//*[@id="main"]/div/table/tbody/tr[11]/td/table/tbody/tr/td[2]/table/tbody/tr/td[4]/div/span[2]').text.strip()
+        print(USN+str(i).zfill(3)+"\t"+sgpa)
+    except:
+        print("USN: "+USN+str(i).zfill(3)+" Doesnt Exist")
+    finally:
+        driver.back()
+driver.close()
